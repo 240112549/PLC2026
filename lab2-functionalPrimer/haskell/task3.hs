372dff3730a5a510d29edeb4bc6e96b1ac1c0674
240112549 <240112549@aston.ac.uk>
2026-02-03 11:19:35 +0000

    Task 2 attempted

--- lab2-functionalPrimer/haskell/task3.hs
+++ lab2-functionalPrimer/haskell/task3.hs
@@ -1,3 +1,17 @@
+grade score =
+ if score >= 90
+  then "A"
+  else if score >= 80
+   then "B"
+   else if score >= 70
+    then "C"
+    else "F"
+
+gradeGuards score   | score >= 90 = "A"
+                    | score >= 80 = "B"
+                    | score >= 70 = "C"
+                    | otherwise = "F"
+
 sgn x = if x < 0 
           then -1 
           else if x == 0 
@@ -12,4 +26,8 @@ main = do
     putStrLn "Please enter a number"
     input <- getLine 
     let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
+    putStrLn( show (sgnGuards (x)) )
+    putStrLn "Please enter your grade"
+    bee <- getLine
+    let score = (read bee :: Int)
+    putStrLn( show (gradeGuards (score)) )
